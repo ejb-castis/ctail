dd8449139ac3935263612144f47a7adfb94d64c2
ejb <ejb@castis.com>
2020-11-03 18:22:56 +0900

    update

--- ctail_test.py
+++ ctail_test.py
@@ -2,40 +2,68 @@ import os
 import unittest
 import jtail as ctail
 
+
 class CtailTests(unittest.TestCase):
-  def setUp(self):
-    self.dir = "unittest/test"
-    try:
-      exist = os.path.exists(self.dir)
-      if exist == False:
-        os.makedirs(self.dir)
-    except OSError as e:
-        print "error : %s, %s" % (self.dir, e)
-    else:
-        print "made dir : %s" % (self.dir)
+    def setUp(self):
+        self.dir = "unittest/test"
+        self.file_name = "test.txt"
+        self.file_path = self.dir + "/" + self.file_name
+        try:
+            exist = os.path.exists(self.dir)
+            if exist == False:
+                os.makedirs(self.dir)
+        except OSError as e:
+            print ("error : {}, {}".format(self.dir, e))
+        else:
+            print ("made dir : {}".format(self.dir))
 
-  def tearDown(self):
-    os.removedirs(self.dir)
-    print "removed dir : %s" % (self.dir)
+    def tearDown(self):
+        os.remove(self.file_path)
+        os.removedirs(self.dir)
+        print ("removed dir : {}".format(self.dir))
+        pass
 
-  # -f, 파일 크기가 2048보다 작은 경우
-  def test_the_file_deleted_while_tailing_a_file(self):
-        expected = True
-        result = True
-        self.assertEqual(result, expected)
+    def newFile(self):
+        try:
+            f = open(self.file_path, "w")
+            print >> f, "line:1"
+        finally:
+            if f is not None:
+                f.close()
+
+    def appendFile(self):
+        try:
+            f = open(self.file_path, "aw")
+            print >> f, "line:1"
+        finally:
+            if f is not None:
+                f.close()
+
+    # -f, 파일 크기가 2048보다 작은 경우
+    def test_the_file_deleted_while_tailing_a_file(self):
+        self.newFile()
+
+        f = None
+        try:
+          ctail._verbose = False
+
+          f, error = ctail.open_tail(self.file_path)
+          self.assertEqual(False, error)
+
+
+          offset = f.tell()
+          #print("file path:{}, offset:{}".format(self.file_path, offset))
+          #제일 처음 부터 읽어들여야 함
+          self.assertEqual(0, offset)
+
+        except Exception as e:
+          self.assertTrue(False, "error, {}".format(e))
+        finally:
+          if f is not None:
+            f.close()
 
 
 # -f, tail 중인 파일이 지워지는 경우
-  # def test_the_file_deleted_while_tailing_a_file(self):
-  #     code = r"""
-  #     a
-  #     """
-  #     expected = r"""
-  #     a;
-  #     """
-  #     result = el.add_semicolon_to_end_of_statement(code)
-  #     self.assertEqual(result, expected)
-
 
 if __name__ == '__main__':
     unittest.main()
